0ebcf31376e850e51be1814a0327bb9ca4e37b5b
LordDudee <michael_runschke@gmx.de>
2021-12-16 11:16:09 +0000

    class blockchain ++

--- blockchain.js
+++ blockchain.js
@@ -3,10 +3,14 @@ const Block = require("./block");
 class Blockchain{
 
     constructor(){
-        this.chain = [];
+        this.chain = [Block.genesis()];  // Am Anfang war ...
     }
 
-
+    addBlock(data){   // Block erzeugen und an chain anhängen
+        const block = Block.mineBlock(this.chain[this.chain.length-1],data);
+        this.chain.push(block);  // block an Kette anhängen
+        return block; // monitoring
+    }
 
 
 }
