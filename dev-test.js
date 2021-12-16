1fa09cab58e8d5d380f84cb00a71ea1fe9e7ad17
LordDudee <michael_runschke@gmx.de>
2021-12-16 11:58:53 +0000

    Blockchain Block 1-4

--- dev-test.js
+++ dev-test.js
@@ -1,9 +1,27 @@
-const Block = require("./block");
+const Block = require ("./block");
+const Blockchain = require("./blockchain");
+//const block = new Block("time","hash1","hash2","mydata");
 
-// const block = new Block("time","hash1","hash2","data");
-// console.log(block.toString());
 
-//console.log(Block.genesis().toString());
+//console.log(block.genesis().toString());
+// const testBlock = Block.mineBlock(Block.genesis(),"test");
+// console.log(testBlock.toString());
 
-const testBlock = Block.mineBlock(Block.genesis(),"test");
-console.log(testBlock.toString());
+// 1.Block
+
+const chain = new Blockchain();
+console.log(chain);
+
+// 2.Block
+chain.addBlock("100€ an boo, 50$ von foo");
+console.log(chain);
+
+// +3.Block
+
+chain.addBlock("50€ von boo");
+console.log(chain);
+
+// +4.Block
+
+chain.addBlock("Bank an Kasse");
+console.log(chain);
